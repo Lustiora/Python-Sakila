cf7e77d6b2e671a0d6048d72b5432535f1a09ead
Lustiora <parkjaegun0219@gmail.com>
2026-01-15 13:13:50 +0900

    Login 가능횟수(3) 지정, Window 중앙 정렬 Module 추가

--- GUI_test1.py
+++ GUI_test1.py
@@ -1,15 +1,35 @@
 import tkinter # Python 기본 GUI Package
-
+from tkinter import messagebox # 팝업창 GUI Package
+######################################
+# Login 모듈
+count = 3
 def user_login():
+    global count
     login_id = user_id.get()
     login_pw = user_pw.get()
     print(f"ID : {login_id} | PW : {login_pw}")
     print("DB Connected ...")
+    count = count - 1
     if login_id == "lus" or login_pw == "tiger": # 일치조건 > DB 연동 예정
         print("Login Successful")
+        messagebox.showinfo("Login", "Login Successful")
         login.destroy() # Window (Login) 종료
     else:
         print("Login Failed")
+        messagebox.showinfo("Login", f"Login Failed\nChance(3) : {count}")
+    if count == 0:
+        messagebox.showinfo("Login", "Please Contact the Administrator")
+        print("Not Connected")
+        login.destroy()
+######################################
+# Window 자동 중앙 정렬 모듈 (미정렬 시 좌측 상단)
+def center_window(window, width, height):
+    screen_width = window.winfo_screenwidth() # 현재 모니터의 해상도(크기)를 가져옴
+    screen_height = window.winfo_screenheight()
+    x_pos = (screen_width // 2) - (width // 2) # 정중앙 좌표 계산 \ (//)는 정수 나누기
+    y_pos = (screen_height // 2) - (height // 2)
+    window.geometry(f"{width}x{height}+{x_pos}+{y_pos}") # 위치 적용 (가로x세로+X좌표+Y좌표)
+# 적용 > center_window([tkinter Window],[width],[height])
 ######################################
 # 변수명 지정 없이 tkinter.Tk()로 작성하면 해당하는 속성이 포함된 창 3개가 출력
 # tkinter.Tk().title("DVD")
@@ -21,8 +41,8 @@ def user_login():
 ######################################
 # Window (Login)
 login = tkinter.Tk() # 표사되는 Window(tkinter.Tk())에 변수명을 지정하여 변수명을 기준으로 속성을 추가
-login.title("Login")
-login.geometry("260x100")
+login.title("Sakila Login")
+center_window(login, 260, 100)
 ######################################
 # Login 화면 구성 grid
 # row=[행], column=[열]) > 0행 0열 = 좌측 상단 / 행과 열이 겹치는 경우 덮어씌워짐
